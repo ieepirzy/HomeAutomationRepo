dc24ff0291c66bb1c4b22465e0a8719cf969686a
ieepirzy <ilari.e.pirkkalainen@student.jyu.fi>
2025-08-31 13:56:09 +0300

    fixed small syntax issue with checking if environment variables loaded

--- LgOsControl/Tv_Script.py
+++ LgOsControl/Tv_Script.py
@@ -10,7 +10,7 @@ async def main():
     tv_ip = os.getenv("Tv_IP")
     clientKey = os.getenv("CLIENT_KEY")
 
-    if not "tv_ip" or not clientKey:
+    if not tv_ip or not clientKey:
         raise ValueError("failed to load 1 or more variables from environment.")
 
     client = WebOsClient(tv_ip, clientKey)
