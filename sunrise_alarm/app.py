4b23a224357a4631173b7c51f3d11023a368cd78
ieepirzy <ilari.e.pirkkalainen@student.jyu.fi>
2025-11-04 19:52:47 +0200

    Added Flask API for apps to fetch sunrise time. Implemented token-based auth; use a high-entropy key.

--- sunrise_alarm/app.py
+++ sunrise_alarm/app.py
@@ -1,5 +1,12 @@
 import requests
 import zoneinfo
+from flask import Flask, jsonify, request, abort
+import os
+
+
+
+app = Flask(__name__)
+TOKEN = os.getenv("AUTH_TOKEN")
 
 def get_sunrise(lat,long,tz):
     url = "https://api.sunrise-sunset.org/json"
@@ -22,16 +29,31 @@ def get_timezone_from_coords(lat, lng):
     res.raise_for_status()
     return res.json()["timeZone"]
 
-def get_coords_from_ip():
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
-    res = requests.get("https://ipapi.co/json/", headers=headers, timeout=10)
-    res.raise_for_status()
-    data = res.json()
-    return data["latitude"], data["longitude"]
+#def get_coords_from_ip():
+#    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+#    res = requests.get("https://ipapi.co/json/", headers=headers, timeout=10)
+#    res.raise_for_status()
+#    data = res.json()
+#    return data["latitude"], data["longitude"]
+
+#lat, lng = get_coords_from_ip() #used for dynamic location finding
+lat, lng = 61.49911,23.78712 #Hardcoded coordinates for Tampere due to the VM I am deploying to being in the United States.
+tz_name = get_timezone_from_coords(lat, lng)
+tz = zoneinfo.ZoneInfo(tz_name) 
+
+@app.route("/api/v1/sunrise", methods=["GET"])
+def sunrise():
+    auth = request.headers.get("Authorization", "")
+    if not auth.startswith("Bearer "):
+        abort(401)
+    token = auth.split(None, 1)[1]
+    if token != TOKEN:
+        abort(401)
+
+    data = get_sunrise(lat, lng, f"{tz}")
+    print(f"Sunrise at: {data}, Your timezone: {tz}")
+    return jsonify({"sunrise": data})
+
 
 if __name__ == "__main__":
-    lat, lng = get_coords_from_ip()
-    tz_name = get_timezone_from_coords(lat, lng)
-    tz = zoneinfo.ZoneInfo(tz_name)
-    sunrise_iso = get_sunrise(lat, lng, f"{tz}")
-    print(f"Sunrise at: {sunrise_iso}, Your timezone: {tz}")
+    app.run(host="0.0.0.0", port=5000)
